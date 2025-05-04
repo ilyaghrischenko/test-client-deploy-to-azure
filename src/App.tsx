67f01ba270bfbfc145fc8c7965b6ad4c36e4e6b1
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-05-04 17:22:32 +0300

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
 import {Azure} from "./Azure.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [count, setCount] = useState<number>(0);
+
+    const [messageFromApi, setMessageFromApi] = useState<string>('');
+    const fetchMessage = async () => {
+        const response = await fetch('https://test-api-123456.azurewebsites.net/message');
+        const data: string = await response.json();
+
+        setMessageFromApi(data);
+    };
+
+    useEffect(() => {
+        fetchMessage();
+    }, []);
 
   return (
       <>
@@ -13,6 +25,8 @@ function App() {
 
           <button onClick={() => setCount(count + 1)}>+</button>
           <button onClick={() => setCount(count - 1)}>-</button>
+
+          <h3>Message form api: {messageFromApi}</h3>
       </>
   )
 }
